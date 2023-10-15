28cd01f39f88a5ca166cb020245dd586a7be9bfe
Tyrn <a@b.com>
2023-10-15 20:56:15 +0300

    fmt source code formatting script added

--- tests/test_fib.cpp
+++ tests/test_fib.cpp
@@ -2,8 +2,8 @@
 
 #include "fibonacci.hpp"
 
-TEST_CASE("Simple test case calculating fibonacci for 10th element","[fibonacci]") {
-    REQUIRE(fibonacci(9) == 34);
-    REQUIRE(fibonacci(10) == 55);
+TEST_CASE("Simple test case calculating fibonacci for 10th element",
+          "[fibonacci]") {
+  REQUIRE(fibonacci(9) == 34);
+  REQUIRE(fibonacci(10) == 55);
 }
-
